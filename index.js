7b0f0bdf0fb20bc6f1ff34b060d7bfc010ae500d
Milk-Cool <milan-pl@mail.ru>
2023-08-16 18:17:45 +0300

    added map

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 
 const appMain = require("./leto-main-page");
+const appMap = require("./leto-map");
 
 const app = express();
 const port = 3000;
@@ -8,5 +9,6 @@ const port = 3000;
 app.use("/res", express.static("leto-resources"));
 
 app.use(appMain);
+app.use(appMap);
 
 app.listen(port);

4089c556336f1d6d28bec86cc7b472f8978587ae
Milk-Cool <milan-pl@mail.ru>
2023-08-18 17:56:54 +0300

    modified:   index.js 	modified:   leto-resources

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 
 const appMain = require("./leto-main-page");
 const appMap = require("./leto-map");
+const appSites = require("./leto-sites");
 
 const app = express();
 const port = 3000;
@@ -10,5 +11,6 @@ app.use("/res", express.static("leto-resources"));
 
 app.use(appMain);
 app.use(appMap);
+app.use(appSites);
 
 app.listen(port);

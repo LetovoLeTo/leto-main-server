2ebdeb7f7c484f52e0dc645a9197a9b2b41daa55
Milk-Cool <milan-pl@mail.ru>
2023-08-23 20:14:51 +0300

    this is just stupid

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const appMain = require("./leto-main-page");
 const appMap = require("./leto-map");
 const appSites = require("./leto-sites");
+const appHomework = require("./leto-homework");
 
 const app = express();
 const port = 3000;
@@ -12,5 +13,6 @@ app.use("/res", express.static("leto-resources"));
 app.use(appMain);
 app.use(appMap);
 app.use(appSites);
+app.use(appHomework);
 
 app.listen(port);

8321a291b836b68dff82ff3254bf2d7b0a016389
Milk-Cool <milan-pl@mail.ru>
2023-08-31 17:12:14 +0300

    update submodules

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const appMain = require("./leto-main-page");
 const appMap = require("./leto-map");
 const appSites = require("./leto-sites");
 const appHomework = require("./leto-homework");
+const appTimetables = require("./leto-timetables");
 
 const app = express();
 const port = 3000;
@@ -12,8 +13,9 @@ const port = 3000;
 app.use("/res", express.static(join(__dirname, "leto-resources")));
 
 app.use(appMain);
-app.use(appMap);
+app.use(appTimetables);
 app.use(appSites);
 app.use(appHomework);
+app.use(appMap);
 
 app.listen(port);

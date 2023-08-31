565a7a3750b4a5f8db113a65b935d71c26aba5fa
Milk-Cool <milan-pl@mail.ru>
2023-08-31 16:43:26 +0300

    removed timetables from main file

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const appMain = require("./leto-main-page");
 const appMap = require("./leto-map");
 const appSites = require("./leto-sites");
 const appHomework = require("./leto-homework");
-const appTimetables = require("./leto-timetables");
 
 const app = express();
 const port = 3000;
@@ -16,6 +15,5 @@ app.use(appMain);
 app.use(appMap);
 app.use(appSites);
 app.use(appHomework);
-app.use(appTimetables);
 
 app.listen(port);

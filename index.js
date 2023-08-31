a9e2f1127cea34fcf64ecd123e1e6ec3f84088bd
Milk-Cool <milan-pl@mail.ru>
2023-08-31 17:18:12 +0300

    added that one warning

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const port = 3000;
 
 app.use("/res", express.static(join(__dirname, "leto-resources")));
 
+// DO NOT CHANGE THE ORDER IN WHICH THE MODULES ARE BEING USED!
 app.use(appTimetables);
 app.use(appSites);
 app.use(appMap);
